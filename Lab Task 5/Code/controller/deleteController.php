5950589e7f9fbade7af12b27cd1d59e4538da5fb
SufianMustaq <72722597+SufianMustaq@users.noreply.github.com>
2021-04-09 20:32:59 +0600

    LAB TASK 5
    LAB TASK 5

---
+++ Lab Task 5/Code/controller/deleteController.php
@@ -0,0 +1,14 @@
+<?php if(!empty($_GET['id'])){ ?>
+<?php 
+
+require_once '../model/model.php';
+$tableName = 'product';
+//echo $_GET['id'];
+if (deleteProduct($tableName, $_GET['id'])) {
+    header('Location: ../display.php');
+}
+else{
+	echo '<p>Product Not Deleted!!</p>';
+}
+ ?>
+<?php }
